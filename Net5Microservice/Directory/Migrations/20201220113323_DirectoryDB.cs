1f56627a28563e2c70c6cd5753b981423a0d136c
akinci41 <ersin.akinci@hotmail.com>
2020-12-20 14:36:03 +0300

    added docker compose to setup postgres and migrations

---
+++ Net5Microservice/Directory/Migrations/20201220113323_DirectoryDB.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Directory.Migrations
+{
+    public partial class DirectoryDB : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Contacts",
+                columns: table => new
+                {
+                    ID = table.Column<Guid>(type: "uuid", nullable: false),
+                    Name = table.Column<string>(type: "varchar(50)", nullable: true),
+                    Surname = table.Column<string>(type: "varchar(100)", nullable: true),
+                    FirmName = table.Column<string>(type: "varchar(100)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Contacts", x => x.ID);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Communications",
+                columns: table => new
+                {
+                    ID = table.Column<Guid>(type: "uuid", nullable: false),
+                    ContactID = table.Column<Guid>(type: "uuid", nullable: false),
+                    Type = table.Column<string>(type: "char(1)", nullable: true),
+                    Content = table.Column<string>(type: "varchar(250)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Communications", x => x.ID);
+                    table.ForeignKey(
+                        name: "FK_Communications_Contacts_ContactID",
+                        column: x => x.ContactID,
+                        principalTable: "Contacts",
+                        principalColumn: "ID",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Communications_ContactID",
+                table: "Communications",
+                column: "ContactID");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Communications");
+
+            migrationBuilder.DropTable(
+                name: "Contacts");
+        }
+    }
+}
--- Net5Microservice/Directory/Startup.cs
+++ Net5Microservice/Directory/Startup.cs
@@ -1,15 +1,10 @@
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
 using Microsoft.OpenApi.Models;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 
 namespace Directory
 {
@@ -25,6 +20,8 @@ namespace Directory
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddMvc();
+            services.AddEntityFrameworkNpgsql().AddDbContext<Entity.DirectoryContext>(x => x.UseNpgsql(Configuration.GetConnectionString("DefaultConnection")));
 
             services.AddControllers();
             services.AddSwaggerGen(c =>
