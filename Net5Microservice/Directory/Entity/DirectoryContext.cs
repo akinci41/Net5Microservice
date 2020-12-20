7b442a960961a234cd492904458b9992cb1141e3
akinci41 <ersin.akinci@hotmail.com>
2020-12-20 14:15:31 +0300

    DB context created

---
+++ Net5Microservice/Directory/Entity/DirectoryContext.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Directory.Entity
+{
+    public class DirectoryContext : DbContext
+    {
+        public DirectoryContext(DbContextOptions<DirectoryContext> options) : base(options) { }
+        public DbSet<Entity.Contact> Contacts { get; set; }
+        public DbSet<Entity.Communication> Communications { get; set; }
+    }
+}
